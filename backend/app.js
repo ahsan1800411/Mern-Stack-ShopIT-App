146c23cc73ff5a40f2d0b4dfea0b3dab29f955fb
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 16:42:20 +0500

    Protected routes from unauthorized users

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const product = require("./routes/product");
 const user = require("./routes/user");
+const cookieParser = require("cookie-parser");
 
 const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
+app.use(cookieParser());
 
 //  import all the routes
 app.use("/api/v1", product);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,8 +6,9 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productControllers");
+const { isAuthenticatedUser } = require("../middlewares/auth");
 
-router.route("/products").get(getProducts);
+router.route("/products").get(isAuthenticatedUser, getProducts);
 router.route("/admin/product/new").post(newProduct);
 router.route("/product/:id").get(getSingleProduct);
 router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
