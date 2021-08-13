993e58686dc5b25ece2681b1f659a50fe6de1fd9
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 09:00:28 +0500

    Get a single product by its id route

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -19,3 +19,18 @@ exports.getProducts = async (req, res, next) => {
     products,
   });
 };
+
+//  get a single product ==> /api/v1/product/:id == get request
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product)
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  res.status(200).json({
+    success: true,
+
+    product,
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,13 @@
 const {
   getProducts,
   newProduct,
+  getSingleProduct,
 } = require("../controllers/productControllers");
 
 const router = require("express").Router();
 
 router.route("/products").get(getProducts);
 router.route("/product/new").post(newProduct);
+router.route("/product/:id").get(getSingleProduct);
 
 module.exports = router;
