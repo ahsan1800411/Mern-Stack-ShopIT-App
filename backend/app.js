44c003b0c440a0a2486868a7210b2a2f7dd70cc2
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 11:19:12 +0500

    Create a Error Middleware to catch the errors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const product = require("./routes/product");
+const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 
 //  import all the routes
 app.use("/api/v1", product);
+// Middleware to catch the errors
+app.use(errorMiddleware);
 
 module.exports = app;
