c1d7cd4256c7f593bc044f21ef09c707d4ee4f40
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 12:23:40 +0500

    Added the filter Functionality

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -15,6 +15,20 @@ class ApiFeatures {
     this.query = this.query.find({ ...keyword });
     return this;
   }
+  //   Filter Functionality
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    // remove Fields
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    // Advanced Filters for price and ratings
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
 module.exports = ApiFeatures;
