cb14e640307b493dcfe0a1b04848db057e1ddc34
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 11:27:01 +0500

    Production And Development Errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -3,8 +3,21 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.messages = err.messages || "Internal Server Error";
 
-  res.status(err.statusCode).json({
-    success: false,
-    error: err.stack,
-  });
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(err.statusCode).json({
+      sucess: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    let { ...error } = err;
+    error.message = err.message;
+    res.status(errpr.statusCode).json({
+      success: false,
+      error: error.message || "Internal Server Error",
+    });
+  }
 };
