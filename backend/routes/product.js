1f77b00364a8bf3c8b1b385ff00b294ed67acdf2
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 08:10:25 +0500

    Exports the router

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,3 +3,5 @@ const { getProducts } = require("../controllers/productControllers");
 const router = require("express").Router();
 
 router.route("/products").get(getProducts);
+
+module.exports = router;
