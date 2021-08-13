1d8d40074610e38274f2c9a2ebac5e2add353a3a
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 08:50:06 +0500

    Add the file of all products

--- backend/utils/seeder.js
+++ backend/utils/seeder.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const dotenv = require("dotenv");
 const connectDatabase = require("../config/database");
+const products = require("../data/products.json");
 
 dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
@@ -9,7 +10,7 @@ const seedProducts = async () => {
   try {
     await Product.deleteMany();
     console.log("All the Products are deleted");
-    await Product.insertMany();
+    await Product.insertMany(products);
     console.log("All the products are inserted");
     process.exit();
   } catch (error) {
