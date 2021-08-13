af01eabd7519e3f01e80687987f699abf0d6837c
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-13 09:07:43 +0500

    Update a product route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,12 +2,14 @@ const {
   getProducts,
   newProduct,
   getSingleProduct,
+  updateProduct,
 } = require("../controllers/productControllers");
 
 const router = require("express").Router();
 
 router.route("/products").get(getProducts);
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
 router.route("/product/:id").get(getSingleProduct);
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
