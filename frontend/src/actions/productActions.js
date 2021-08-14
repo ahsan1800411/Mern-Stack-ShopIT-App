7cffc28cccbf1ceb874f333510aefb5447842cea
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-14 17:03:39 +0500

    Added ProductDetails actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
   ALL_PRODUCTS_REQUEST,
   ALL_PRODUCTS_SUCCESS,
   CLEAR_ERRORS,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstants";
 
 export const getProducts = () => async (dispatch) => {
@@ -22,6 +25,29 @@ export const getProducts = () => async (dispatch) => {
       error: error.response.data.message,
     });
   }
+};
+
+export const getProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAILS_REQUEST,
+    });
+    const { data } = await axios(`/api/v1/product/${id}`);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data.product,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      error: error.response.data.message,
+    });
+  }
+};
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
 };
 
 //   tO clear the errors
