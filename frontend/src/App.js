bf5d1dbc36d757b30d072a028461874b1b432f38
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-14 15:14:18 +0500

    Implement React-Router-dom to the App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
@@ -5,11 +6,16 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <div className='container container-fluid'>
+          <Route path='/' exact component={Home} />
+        </div>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
