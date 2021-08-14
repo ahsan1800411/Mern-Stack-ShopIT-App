db653ed6b644a11b524a1acdc186d194bc2294cc
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-14 16:57:51 +0500

    Adding React-alert

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,16 +4,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
 import Product from "./product/Product";
 import Loader from "./layout/Loader";
-
+import { useAlert } from "react-alert";
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
   );
 
   useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProducts());
-  }, [dispatch]);
+  }, [dispatch, error]);
   return (
     <>
       {loading ? (
