5e65a67568316e6e41689af93a2cd8ed63a951df
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-14 07:30:55 +0500

    Send Password Recovery Email

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const bcrypt = require("bcryptjs");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 // register A NEW user ==> /api/v1/register ==> post request
 
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -34,6 +35,41 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Invalid Email or password", 400));
   }
   sendToken(user, 201, res);
+});
+
+// Forgot password ==> /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler("User not found With this Email", 404));
+  }
+  // Get Reset Password token
+  const resetToken = user.getResetPasswordToken();
+  await user.save({ validateBeforeSave: false });
+
+  // create reset password url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token os as follows :\n\n${resetUrl}\n\nIf you have not requested this email, Please ignore it`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "ShopIT password Recovery",
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorHandler(error.message, 500));
+  }
 });
 
 // logout user ==> /api/v1/logout ==> get request
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,10 +3,12 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  forgotPassword,
 } = require("../controllers/authControllers");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
+router.route("/password/forgot").post(forgotPassword);
 
 module.exports = router;
