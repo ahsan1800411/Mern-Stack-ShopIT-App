75fdeb827686f8373ea71c5126a3765803509d77
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-15 09:26:39 +0500

    Change logo to a Link Component

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Fragment } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Search from "./Search";
 
 const Header = () => {
@@ -9,7 +9,9 @@ const Header = () => {
       <nav className='navbar row'>
         <div className='col-12 col-md-3'>
           <div className='navbar-brand'>
-            <img src='/images/logo192.png' alt='logo' />
+            <Link to='/'>
+              <img src='/images/logo192.png' alt='logo' />
+            </Link>
           </div>
         </div>
 
