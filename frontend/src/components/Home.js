de29d500e7d27306908dc09cc2ec4b806c1dee54
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-15 09:25:49 +0500

    Filter by price

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import MetaData from "./layout/MetaData";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
@@ -7,8 +9,12 @@ import Loader from "./layout/Loader";
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
 
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
 
   const alert = useAlert();
   const dispatch = useDispatch();
@@ -21,8 +27,8 @@ const Home = ({ match }) => {
     if (error) {
       return alert.error(error);
     }
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -38,26 +44,66 @@ const Home = ({ match }) => {
           <h1 id='products_heading'>Latest Products</h1>
           <section id='products' className='container mt-5'>
             <div className='row'>
-              {products &&
+              {keyword ? (
+                <>
+                  <div className='col-6 col-md-3 mt-5 mb-5'>
+                    <div className='px-5'>
+                      <Range
+                        marks={{ 1: `$1`, 1000: `$1000` }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+                  <div className='col-6 col-md-9'>
+                    <div className='row'>
+                      {products &&
+                        products.map((product) => {
+                          return (
+                            <Product
+                              key={product._id}
+                              product={product}
+                              col={4}
+                            />
+                          );
+                        })}
+                    </div>
+                  </div>
+                </>
+              ) : (
+                products &&
                 products.map((product) => {
-                  return <Product key={product._id} product={product} />;
-                })}
+                  return (
+                    <Product key={product._id} product={product} col={3} />
+                  );
+                })
+              )}
             </div>
           </section>
-          <div className='d-flex justify-content-center m-5'>
-            <Pagination
-              activePage={currentPage}
-              itemsCountPerPage={resPerPage}
-              totalItemsCount={productsCount}
-              onChange={setCurrentPageNo}
-              nextPageText={"Next"}
-              prevPageText={"Prev"}
-              firstPageText={"First"}
-              lastPageText={"Last"}
-              itemClass='page-item'
-              linkClass='page-link'
-            />
-          </div>
+          {resPerPage <= productsCount && (
+            <div className='d-flex justify-content-center m-5'>
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Prev"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass='page-item'
+                linkClass='page-link'
+              />
+            </div>
+          )}
         </>
       )}
     </>
