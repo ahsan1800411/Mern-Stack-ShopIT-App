87e704d8323274748f6fe60a98d8c3e6bd7e7794
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-16 11:14:35 +0500

    Register Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 import ProductDetails from "./components/product/ProductDetails";
 import Login from "./components/user/Login";
+import Resgister from "./components/user/Resgister";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/search/:keyword' component={Home} />
           <Route path='/product/:id' exact component={ProductDetails} />
           <Route path='/login' component={Login} />
+          <Route path='/register' component={Resgister} />
         </div>
 
         <Footer />
