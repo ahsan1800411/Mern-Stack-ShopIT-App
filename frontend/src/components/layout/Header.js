f2acbcfd30bfab1a306bdcec332d6e1810984de9
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-18 10:26:30 +0500

    Fix the Import and export

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { Fragment } from "react";
+import React, { Fragment } from "react";
 import { Route, Link } from "react-router-dom";
+
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
+import { logout } from "../../actions/userActions";
+
 import Search from "./Search";
-import { logoutUser } from "../../actions/userActions";
+
+import "../../App.css";
 
 const Header = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
-  const { loading, user } = useSelector((state) => state.auth);
+
+  const { user, loading } = useSelector((state) => state.auth);
+  const { cartItems } = useSelector((state) => state.cart);
 
   const logoutHandler = () => {
-    dispatch(logoutUser());
-    alert.success("Logout Successfully");
+    dispatch(logout());
+    alert.success("Logged out successfully.");
   };
+
   return (
     <Fragment>
       <nav className='navbar row'>
         <div className='col-12 col-md-3'>
           <div className='navbar-brand'>
             <Link to='/'>
-              <img src='/images/logo192.png' alt='logo' />
+              <img src='/images/shopit_logo.png' />
             </Link>
           </div>
         </div>
@@ -36,9 +42,10 @@ const Header = () => {
               Cart
             </span>
             <span className='ml-1' id='cart_count'>
-              2
+              {cartItems.length}
             </span>
           </Link>
+
           {user ? (
             <div className='ml-4 dropdown d-inline'>
               <Link
@@ -64,16 +71,14 @@ const Header = () => {
                 className='dropdown-menu'
                 aria-labelledby='dropDownMenuButton'
               >
-                {user && user.role !== "admin" ? (
-                  <Link className='dropdown-item' to='/orders/me'>
-                    Orders
-                  </Link>
-                ) : (
+                {user && user.role === "admin" && (
                   <Link className='dropdown-item' to='/dashboard'>
                     Dashboard
                   </Link>
                 )}
-
+                <Link className='dropdown-item' to='/orders/me'>
+                  Orders
+                </Link>
                 <Link className='dropdown-item' to='/me'>
                   Profile
                 </Link>
