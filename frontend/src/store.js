0d53509e297a100295d7681c711f946d93b65d6d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-18 10:00:56 +0500

    Add to cart and order functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,30 +1,46 @@
-import { combineReducers, applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import {
-  productsReducer,
-  productDetailsReducer,
-} from "./reducers/productsReducer";
-import {
-  authReducer,
-  userReducer,
-  forgotPasswordReducer,
-} from "./reducers/userReducers";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import { productsReducer, newProductReducer, productReducer, productDetailsReducer, newReviewReducer, productReviewsReducer, reviewReducer } from './reducers/productReducers'
+import { authReducer, userReducer, forgotPasswordReducer, allUsersReducer, userDetailsReducer } from './reducers/userReducers'
+import { cartReducer } from './reducers/cartReducers'
+import { newOrderReducer, myOrdersReducer, orderDetailsReducer, allOrdersReducer, orderReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
-  products: productsReducer,
-  productDetails: productDetailsReducer,
-  auth: authReducer,
-  user: userReducer,
-  forgotPassword: forgotPasswordReducer,
-});
-const middlewares = [thunk];
+    products: productsReducer,
+    productDetails: productDetailsReducer,
+    newProduct: newProductReducer,
+    product: productReducer,
+    productReviews: productReviewsReducer,
+    review: reviewReducer,
+    auth: authReducer,
+    user: userReducer,
+    allUsers: allUsersReducer,
+    userDetails: userDetailsReducer,
+    forgotPassword: forgotPasswordReducer,
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    allOrders: allOrdersReducer,
+    orderDetails: orderDetailsReducer,
+    order: orderReducer,
+    newReview: newReviewReducer
+})
 
-const initialState = {};
-const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middlewares))
-);
+
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+        shippingInfo: localStorage.getItem('shippingInfo')
+            ? JSON.parse(localStorage.getItem('shippingInfo'))
+            : {}
+    }
+}
+
+const middlware = [thunk];
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlware)))
 
 export default store;
